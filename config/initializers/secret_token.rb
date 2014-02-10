f48b5581a9f208076c32014f141d6e0e0a78be6d
ChanellJ <cjackson@chicagotechacademy.org>
2014-02-10 13:19:36 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TDDWCApp::Application.config.secret_key_base = '4f88bc1f88f592ed3b7e62c2f1adf2b4d96cefb8c95c2e4de8eae2673da91148b3b5bf5c45fd5fa263aeef424b3b37ed69460dcfd155b680370b386cc4ff1ece'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TDDWCApp::Application.config.session_store :cookie_store, key: '_TDDWC_app_session'
